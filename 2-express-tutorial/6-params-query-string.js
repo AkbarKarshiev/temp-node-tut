840eb80083f0f585adf275349e94abb4551e707e
Akbar Karshiev <akbar.karshiev@gcore.lu>
2024-08-31 21:27:23 +0500

    7-route-params

--- 2-express-tutorial/6-params-query-string.js
+++ 2-express-tutorial/6-params-query-string.js
@@ -11,17 +11,6 @@ app.get('/api/products', (req, res) => {
   res.json(products.map(({desc, ...product}) => product));
 });
 
-app.get('/api/products/:productId', (req, res) => {
-  const { productId } = req.params;
-
-  const foundProduct = products.find((product) => product.id === Number(productId));
-  
-  if (foundProduct)
-    return res.json(foundProduct);
-
-  return res.status(404).json('resource not found');
-});
-
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
 });
